78d3259e2e32c6a41883650c996e8c1c31e86bf0
Matt Stump <mstump@sourceninja.com>
2013-01-13 22:24:48 -0800

    added tests for the ready message

---
+++ test/cql_message_ready.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "cql.h"
+#include "../src/cql_message_ready.hpp"
+
+
+TEST(cql_message_ready_cpp, zero_size)
+{
+	cql::cql_message_ready_t m;
+	EXPECT_EQ(0, m.size());
+}
+
+TEST(cql_message_ready_cpp, opcode)
+{
+	cql::cql_message_ready_t m;
+	EXPECT_EQ(CQL_OPCODE_READY, m.opcode());
+}
+
+TEST(cql_message_ready_cpp, zero_length_serialization)
+{
+	std::stringstream output;
+	cql::cql_message_ready_t m;
+	m.write(output);
+    EXPECT_EQ(0, output.str().length());
+}
