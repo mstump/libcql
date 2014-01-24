7e74cebe32bd29e32f91d7af99f74eb915780d81
Dominic Letz <dominicletz@exosite.com>
2014-01-24 10:34:39 +0800

    Replacing broken small_indexed_storage impl. Fixes crashes

--- include/libcql/internal/cql_callback_storage.hpp
+++ include/libcql/internal/cql_callback_storage.hpp
@@ -21,6 +21,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <vector>
 #include <new>
 
 namespace cql {
@@ -28,138 +29,79 @@ namespace cql {
     template<typename value_t>
     class small_indexed_storage {
     private:
-        template <typename _value_t>
-        class entry_t {
-            struct {
-                struct {
-                    // -1 - there is no next free index
-                    // -2 - flags that his one has been allocated (for checks and stuff)
-                    int32_t index; // signed because might be -1
-                    int32_t count; // doesn't matter never should be negative (see index restrictions)
-                } next_free;
-                _value_t value;
-            } e;
-
-        public:
-            _value_t&
-            value()
-            {
-                return e.value;
-            }
-
-            const _value_t&
-            value() const
-            {
-                return e.value;
-            }
-
-            int8_t
-            next_free_index() const
-            {
-                return e.next_free.index;
-            }
-
-            // not counting this (so 0 means there are no free blocks behind this one)
-            int8_t
-            next_free_cnt() const
-            {
-                return e.next_free.count;
-            }
-
-            // count does not include this (so 0 means there are no free blocks behind this one)
-            // you won't need to set cnt in any case except initial allocation
-            void
-            set_next_free(int32_t index,
-                          int32_t cnt = 0)
-            {
-                e.next_free.index = index;
-                e.next_free.count = cnt;
-            }
-
-            void
-            set_value(const _value_t& val)
-            {
-                e.value = val;
-            }
-
-            bool
-            is_allocated()
-            {
-                return e.next_free.index == -2;
-            }
-
-            void
-            set_allocated()
-            {
-                e.next_free.index = -2;
-            }
+        template<typename t>
+        struct bucket {
+            t value;
+            int is_allocated;
         };
-
-        typedef entry_t<value_t> array_entry_t;
-        array_entry_t* array;
-        int32_t next_free_index;
+        
+        std::vector< bucket<value_t>* > array;
+        std::vector<int32_t> frees;
     public:
-        explicit
-        small_indexed_storage(uint16_t size) :
-                next_free_index(0)
+        small_indexed_storage(int32_t size)
         {
-            array = new array_entry_t[size];
-            array[0].set_next_free(-1, size-1);
+            array.reserve(size);
         }
-
         ~small_indexed_storage()
         {
-            delete [] array;
+            for(size_t i=0; i<array.size(); i++)
+            {
+                release(i);
+                free(array[i]);
+            }
         }
 
-        int32_t
+        int8_t
         allocate()
         {
-            int32_t result;
-            if ( (result = next_free_index) >= 0) {
-                if (array[next_free_index].next_free_cnt() > 0) {
-                    array[++next_free_index].set_next_free(array[result].next_free_index(), array[result].next_free_cnt()-1);
-                }
-                else {
-                    next_free_index = array[next_free_index].next_free_index();
-                }
-                // mark it allocated
-                array[result].set_allocated();
+            if (frees.size()) {
+                int32_t ret = frees.back();
+                frees.pop_back();
+                return ret;
+            } else {
+                bucket<value_t>* ptr = (bucket<value_t>*)calloc(1, sizeof(bucket<value_t>));
+                array.push_back(ptr);
+                return array.size() - 1;
             }
-
-            return result;
         }
 
         void
         release(int32_t index)
         {
-            array[index].set_next_free(next_free_index);
-            next_free_index = index;
+            if (array[index]->is_allocated) {
+                array[index]->value.~value_t();
+                array[index]->is_allocated = 0;
+            }
+            frees.push_back(index);
         }
 
         bool
         has(int32_t index) const
         {
-            return array[index].is_allocated();
+            return (int32_t)array.size() > index && array[index]->is_allocated;
         }
 
         value_t&
         get(int32_t index)
         {
-            return array[index].value();
+            return array[index]->value;
         }
 
         const value_t&
         get(int32_t index) const
         {
-            return array[index].value();
+            return array[index]->value;
         }
 
         void
         put(int32_t index,
             const value_t& val)
         {
-            array[index].set_value(val);
+            if (array[index]->is_allocated) {
+                array[index]->value.~value_t();
+            }
+            array[index]->is_allocated = 1;
+            new (&array[index]->value) value_t(val);
         }
 
     };
